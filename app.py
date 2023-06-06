aa632c62509c20cab497fb59f824cd0d8139226a
void4 <void4@users.noreply.github.com>
2023-06-06 09:58:01 +0200

    Close database connection after request, since not closing in debug mode can result in too many open files when having many requests

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, g
+
 
 class CustomFlask(Flask):
-	jinja_options = Flask.jinja_options.copy()
-	jinja_options.update(dict(
-		variable_start_string='%%',  # Default is '{{', I'm changing this because Vue.js uses '{{' / '}}'
-		variable_end_string='%%',
-	))
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        variable_start_string='%%',  # Default is '{{', I'm changing this because Vue.js uses '{{' / '}}'
+        variable_end_string='%%',
+    ))
+
 
 app = CustomFlask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.after_request
+def after_request(response):
+    if hasattr(g, "_database"):
+        g._database.close()
+        del g._database
+    return response
